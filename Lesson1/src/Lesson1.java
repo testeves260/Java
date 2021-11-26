2a683e8231d6ef237bc62deccf8d740e2efba356
linger91 <testeves@hotmail.com>
2021-11-26 21:28:52 +0000

    Started to work on lesson 1.

--- Lesson1/src/Lesson1.java
+++ Lesson1/src/Lesson1.java
@@ -2,9 +2,20 @@
 public class Lesson1 {
 
 	public static void main(String[] args) {
+
+		int x = 5;
+		int y;
+		int z = 3;
 		
-		System.out.println("Hello World");
-
+		y = 10;
+		
+		System.out.println(y);
+	
+		y = 5;		
+		System.out.println(y);
 	}
-
+	
+	public static String main() {
+		
+	}
 }
