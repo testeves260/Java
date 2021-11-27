33b4513faeb210f46bc152fe0d4fb7844c81f09d
linger91 <testeves@hotmail.com>
2021-11-27 00:19:25 +0000

    commited the second lesson for JAVA

---
+++ Lesson2/src/Lesson2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Lesson2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		String x;
+		int y;
+		double z;
+		
+		x = sc.next();
+		y = sc.nextInt();
+		z = sc.nextDouble();
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+		sc.close();
+	}
+
+}
