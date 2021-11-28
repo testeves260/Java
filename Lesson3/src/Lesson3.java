35f83608cec3b326abb9a2fbfbb06a1fc3556966
linger91 <testeves@hotmail.com>
2021-11-28 17:45:22 +0000

    Exercise 1 - If conditionals

--- Lesson3/src/Lesson3.java
+++ Lesson3/src/Lesson3.java
@@ -10,43 +10,21 @@ public class Lesson3 {
 		exercise1();
 	}
 	static void exercise1() {
-	
+		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Insira o numero do dia da semana 1 - 7");
-		int weekDay = sc.nextInt();
+		System.out.println("Introduza um número inteiro:");
+		int numero = sc.nextInt();
 		
-		switch(weekDay) {
-		
-		case 1:
-			System.out.println("Segunda-Feira");
-			break;
-		case 2:
-			System.out.println("Terça-Feira");
-			break;
-		case 3:
-			System.out.println("Quarta-Feira");
-			break;
-		case 4:
-			System.out.println("Quinta-Feira");
-			break;
-		case 5:
-			System.out.println("Sexta-Feira");
-			break;
-		case 6:
-			System.out.println("Sábado!");
-			break;
-		case 7:
-			System.out.println("Domingo!");
-			break;
+		if (numero > 0 ) {
+			System.out.printf("Número %d é positivo", numero);
+		} else {
+			System.out.printf("Número %d é negativo", numero);
 		}
 		
-		if(weekDay <= 3) {
-			System.out.println("Não desanimes, distrai-te que o tempo corre!");
-		} else if(weekDay == 4) {
-			System.out.println("Anima-te é quase Fim de semana!!");
-		} else if(weekDay == 5) {
-			System.out.println("Hoje é dia de borga!!");
-		}
+		
+	}
+	static void exercise2() {
+		
 	}
 }
