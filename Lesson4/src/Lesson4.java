4c69aad14bbcf6c96be03be3f0bc86785a827068
linger91 <testeves_260@hotmail.com>
2021-11-29 17:26:56 +0000

    Updated exercises

--- Lesson4/src/Lesson4.java
+++ Lesson4/src/Lesson4.java
@@ -6,7 +6,7 @@ public class Lesson4 {
 	public static void main(String[] args) {
 		//Class about repititive sctructures
 		
-		exercise3();
+		exercise4();
 	}
 	
 	static void exercise1() {
@@ -24,6 +24,7 @@ public class Lesson4 {
 		
 		System.out.printf("%nBem vindo Anonymous%n");
 		System.out.println("Setting up workspace.");
+		sc.close();
 	}
 	static void exercise2() {
 		//For loops
@@ -36,35 +37,61 @@ public class Lesson4 {
 		for(int i = 0; i <= x; i++) {
 			
 			if (i % 2 == 1) {
-				System.out.printf("Os numeros in range impares são:%n")
+				System.out.printf("Os numeros in range impares são:%n");
 				System.out.println(i);
 			}
 			
 		}
 		
+		sc.close();
 	}
 	static void exercise3() {
-		
+		//Verificar se os numeros introduzidos estão dentro de um range especifico de 10 - 20.
 		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		int validTries = 5; //ChangeMe
-		
 		int in = 0;
 		int out = 0;
 		
-		for(int i = 0; n < validTries; i++) {
-			if (i >= 10 && i <= 20){
-				in = in + 1;
+		int validTries = 5; //Alterar quantidade de tentativas
+		
+		for (int i = 1; i <= validTries; i++) {
+			System.out.println("Digite um numero inteiro:");
+			int value = sc.nextInt();
+			if(value <= 20 && value >= 10) {
+				in = in +1;
 			} else {
-				out = out + 1;
+				out = out +1;
 			}
 		}
-		
 		
 		System.out.println(in + " in");
 		System.out.println(out + " out");
 		
 		sc.close();
+	}
+	static void exercise4() {
+		//Programa para converter uma temperatura de Celsius para Frhn.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char userChoise;
+		do {
+			System.out.println("Insira a temperatura em Cº para converter para Fº");
+			double userTemp = sc.nextDouble();
+			
+			//Calculate:
+			double f = ((9 * userTemp) / 5 )+ 32;
+			System.out.printf("Conversão %.2f%n", f);
+			
+			System.out.println("Deseja converter novamente? (S/N)");
+			userChoise = sc.next().charAt(0);
+			
+		} while(userChoise == 'S' || userChoise == 's');
+		
+		
+		sc.close();
+	}
+	static void exercise5() {
+		
 	}
 }
